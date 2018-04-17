01ed37def13be9bb39db02da6cfb4f5b600b4852
Emanuel Palm <emanuel.palm@ltu.se>
2018-04-17 11:33:21 +0200

    Take {Party,Token}Set.ts out of {Party,Token}.ts

---
+++ source/main/arrowhead/model/PartySet.ts
@@ -0,0 +1,55 @@
+import { Party } from ".";
+
+/**
+ * A set of `Party` objects.
+ */
+export type PartySet = Party | PartyAll | PartyArray;
+
+/**
+ * The set of all parties.
+ */
+export type PartyAll = null;
+
+/**
+ * An array of `Party` objects.
+ */
+export interface PartyArray {
+    parties: Party[];
+}
+
+/**
+ * Checks whether given `parties` represents a single `Party`.
+ * 
+ * @param set Checked `PartySet`.
+ * @returns Whether given `PartySet` is of type `Party`.
+ */
+export function isParty(set: PartySet): set is Party {
+    return set !== null && (<Party>set).key !== undefined;
+}
+
+/**
+ * Checks whether given `parties` represents all relevant `Party` objects.
+ * 
+ * @param set Checked `PartySet`.
+ * @returns Whether given `PartySet` is of type `PartyAll`.
+ */
+export function isPartyAll(set: PartySet): set is PartyAll {
+    return set === null;
+}
+
+/**
+ * Checks whether given `parties` represents an array of `Party` objects.
+ * 
+ * @param parties Checked `PartySet`.
+ * @returns Whether given `PartySet` is of type `PartyArray`.
+ */
+export function isPartyArray(set: PartySet): set is PartyArray {
+    return set !== null && (<PartyArray>set).parties !== undefined;
+}
+
+/**
+ * @returns A value representing all relevant `Party` objects.
+ */
+export function partyAll(): PartyAll {
+    return null;
+}
