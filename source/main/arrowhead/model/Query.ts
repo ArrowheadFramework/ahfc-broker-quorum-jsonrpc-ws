9e73a9e8a0db40324acf311d9e5951bb2395c477
Emanuel Palm <emanuel.palm@ltu.se>
2018-04-17 11:09:16 +0200

    Update Query docs

--- source/main/arrowhead/model/Query.ts
+++ source/main/arrowhead/model/Query.ts
@@ -1,5 +1,15 @@
 /**
- * A specification of a desired set of `items`.
+ * Specifies properties of a desired set of items.
+ *
+ * # Undefined Implies All
+ *
+ * A `Query` is essentially a set of properties that may or may not be specified
+ * in order to limit the size of a result set. Properties that are not specified
+ * ought to be understood as if a certain restriction has _not_ been imposed,
+ * implying that there should be no reduction in result set size. If, for
+ * example, no `offset` is specified, it must be understood as being `0`, as
+ * that is the only way for the property to not reduce the size of a
+ * corresponding `ResultSet`.
  */
 export interface Query {
     /**
