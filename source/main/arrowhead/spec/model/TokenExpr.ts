671ab9d3b64808c8966bb8a4ff157552217b8aff
Emanuel Palm <emanuel.palm@gmail.com>
2018-05-11 17:16:19 +0200

    Finish isTokenExprSatisfiable() implementation

--- source/main/arrowhead/spec/model/TokenExpr.ts
+++ source/main/arrowhead/spec/model/TokenExpr.ts
@@ -105,25 +105,180 @@ export function isTokenExprQualified(expr: TokenExpr): boolean {
 }
 
 /**
- * Checks whether given `expr` represent a _satisfiable_ `Token` expression.
+ * Checks whether given `expr` is a _satisfiable_ `Token` expression.
+ *
+ * The Boolean satisfiability problem is NP hard, and this function uses a
+ * rather naive approach to checking whether given expressions are indeed
+ * satisfiable. This means that performance will degrade quickly for larger
+ * token expressions. If performance would be very important, then there is
+ * likely plenty of room for improving this implementation. Also, the
+ * application could be made to simply reject larger expressions without
+ * checking them.
  *
  * @param expr Checked `TokenExpr`.
  * @returns Whether given `TokenExpr` is satisfiable.
  */
 export function isTokenExprSatisfiable(expr: TokenExpr): boolean {
-    const [n, clauses] = intoCNF(expr);
-    return solveCNF(n, clauses);
+    const registry = { count: 0, nameToId: new Map<string, number>() };
+    const pef = intoPEF(expr, registry);
+    const nnf = intoNNF(pef);
+    const cnf = intoCNF(nnf);
+    return solveCNF(cnf, registry.count);
 
-    function intoCNF(expr) {
-        return [1, [[1]]]; // TODO.
+    // TokenExpr into Predicate Expression Form (PEF).
+    //
+    // The function transforms the token expression into a simpler nested data
+    // structure, where tokens are replaced with positive integers (>0)
+    // referred to as literals.
+    function intoPEF(expr, registry) {
+        if (isToken(expr)) {
+            let name = expr.kind.replace(/([.:])/, "\\$1");
+            if (expr.id !== undefined) {
+                name += "." + expr.id;
+            } else {
+                // Each `Token` without an `id` is considered a unique variable.
+                name += ":" + registry.count;
+            }
+            let id = registry.nameToId.get(name);
+            if (id === undefined) {
+                id = ++registry.count;
+                registry.nameToId.set(name, id);
+            }
+            return id;
+        }
+        if (isTokenNOT(expr)) {
+            return [expr.kind, intoPEF(expr.item, registry)];
+        }
+        return [expr.kind, expr.items.map(item => intoPEF(item, registry))];
     }
 
-    function solveCNF(n, clauses) {
+    // PEF into Negation Normal Form (NNF).
+    //
+    // Removes NOT expressions by transforming AND and IOR expressions and
+    // negating relevant NOT literals. Substitutes XOR expressions with
+    // equivalent such that use only IOR, NOT and AND constructs. Finally,
+    // replaces the expression identifiers with "&" (AND) and "|" (OR), for the
+    // sake of brevity.
+    //
+    // See: https://en.wikipedia.org/wiki/Conjunctive_normal_form
+    function intoNNF(pef, neg = false) {
+        if (typeof pef === "number") {
+            return neg ? -pef : pef;
+        }
+        switch (pef[0]) {
+            case "__not":
+                return intoNNF(pef[1], !neg);
+
+            case "__and":
+                return [neg ? "|" : "&", pef[1]
+                    .map(item => intoNNF(item, neg))];
+
+            case "__ior":
+                return [neg ? "&" : "|", pef[1]
+                    .map(item => intoNNF(item, neg))];
+
+            case "__xor":
+                return intoNNF(["__and", [
+                    ["__ior", pef[1]],
+                    ["__not", ["__and", pef[1]]]]], neg);
+
+            default:
+                throw new Error("Unreachable!");
+        }
+    }
+
+    // NNF into Conjunctive Normal Form (CNF).
+    //
+    // Flattens the given NNF expression by merging nested ANDs and ORs, as well
+    // as by distributing ORs inwards over ANDs (P OR (Q AND R) ->
+    // (P OR Q) AND (P OR R)). Finally, normalizes the resulting expression to
+    // make it consumable by the solveCNF() function.
+    //
+    // See: https://en.wikipedia.org/wiki/Conjunctive_normal_form
+    function intoCNF(nnf) {
+        return normalize(flatten(nnf));
+
+        function flatten(nnf) {
+            if (isLiteral(nnf)) {
+                return nnf;
+            }
+            const [kind, items] = nnf;
+            switch (kind) {
+                case "&":
+                    return ["&", items.reduce((result, item) => {
+                        item = flatten(item);
+                        return result.concat(isAND(item) ? item[1] : [item]);
+                    }, [])];
+
+                case "|":
+                    if (items.length === 1) {
+                        return flatten(items[0]);
+                    }
+                    for (let i = 0; i < items.length; ++i) {
+                        const item = items[i];
+                        if (isAND(item)) {
+                            const transform = (i === 0)
+                                ? distribute(items[1], item[1], items.slice(2))
+                                : distribute(items[0], item[1], [
+                                    ...items.slice(1, i),
+                                    ...items.slice(i + 1),
+                                ]);
+                            return flatten(transform);
+                        }
+                    }
+                    return ["|", items.map(item => {
+                        return isLiteral(item) ? item : flatten(item[1]);
+                    })];
+
+                default:
+                    throw new Error("Unreachable!");
+            }
+        }
+
+        function distribute(p, Q, T) {
+            const [q, r, ...S] = Q;
+            let transform = ["&", [["|", p, q], ["|", p, r], ...S]];
+            if (T.length > 0) {
+                transform = ["|", transform, ...T];
+            }
+            return transform;
+        }
+
+        function isLiteral(x) {
+            return typeof x === "number";
+        }
+
+        function isAND(x) {
+            return Array.isArray(x) && x[0] === "&";
+        }
+
+        function isOR(x) {
+            return Array.isArray(x) && x[0] === "|";
+        }
+
+        function normalize(root) {
+            if (isLiteral(root)) {
+                return [[root]];
+            }
+            if (isOR(root)) {
+                return [root[1]];
+            }
+            return root[1].map(clause => Array.isArray(clause)
+                ? clause[1]
+                : [clause]);
+        }
+    }
+
+    // Solve CNF clauses, which refer to n different variables.
+    //
+    // Performs a recursive binary tree search of all variable combinations.
+    // Can be improved in many ways for improved performance.
+    function solveCNF(clauses, n) {
         if (n < 1) {
             return true;
         }
         return branch(new Array(n), 0, clauses);
-    
+
         function branch(variables, offset, clauses) {
             if (variables.length <= offset) {
                 return true;
@@ -135,7 +290,7 @@ export function isTokenExprSatisfiable(expr: TokenExpr): boolean {
             variables[offset] = false;
             return evaluate(variables, offset + 1, clauses);
         }
-    
+
         function evaluate(variables, offset, clauses) {
             outer: for (const clause of clauses) {
                 for (const literal of clause) {
@@ -147,7 +302,7 @@ export function isTokenExprSatisfiable(expr: TokenExpr): boolean {
             }
             return branch(variables, offset, clauses);
         }
-    
+
         function isTrueOrUnset(variables, offset, literal) {
             return literal > offset
                 || literal > 0 && variables[literal - 1]
--- source/test/arrowhead/spec/model/TestTokenExpr.ts
+++ source/test/arrowhead/spec/model/TestTokenExpr.ts
@@ -1,5 +1,6 @@
 import * as model from "../../../../main/arrowhead/spec/model";
 import * as unit from "../../../unit";
+import * as util from "util";
 
 export class TestTokenExpr implements unit.Suite {
     public readonly name: string = "TokenExpr";
@@ -12,38 +13,45 @@ export class TestTokenExpr implements unit.Suite {
                     // Token.
                     [1, t("A")],
                     [1, t("A1")],
+                    [1, not(t("A1"))],
+                    [1, not(not(t("A1")))],
 
                     // TokenAND.
+                    [1, and()],
                     [1, and(t("A"))],
                     [1, and(t("A1"))],
                     [1, and(t("A1"), t("A"))],
                     [1, and(t("A1"), t("A1"))],
-                    [0, and(t("A1"), not(t("A1")))],                    
+                    [0, and(t("A1"), not(t("A1")))],
+                    [1, and(t("A"), not(t("A")))],
+                    [1, and(t("A1"), not(not(t("A1"))))],
 
                     // TokenIOR.
+                    [1, ior()],
                     [1, ior(t("A1"))],
                     [1, ior(t("A1"), t("A1"))],
                     [1, ior(t("A1"), t("B1"))],
                     [1, and(ior(t("A1"), t("B1")), not(t("B1")))],
                     [1, and(ior(t("A1"), t("B1"), t("C1")), t("A1"), t("B1"))],
-                    [0, and(ior(t("A1"), t("B1")), not(and(t("A1"), t("B1"))))],                                        
+                    [1, and(ior(t("A1"), t("B1")), not(and(t("A1"), t("B1"))))],
 
                     // TokenXOR.
-                    [1, xor(t("A1"))],
+                    [1, xor()],
+                    [0, xor(t("A1"))],
                     [1, xor(t("A1"), t("B1"))],
                     [1, and(xor(t("A1"), t("B1")), not(t("B1")))],
-                    [0, and(xor(t("A1"), t("B1"), t("C1")), t("A1"), t("B1"))],
+                    [1, and(xor(t("A1"), t("B1"), t("C1")), t("A1"), t("B1"))],
                     [0, and(xor(t("A1"), t("B1")), not(t("A1")), not(t("B1")))],
                 ];
 
                 for (const pair of pairs) {
-                    const expected = pair[0] === 1;
+                    const isSatisfiable = pair[0] === 1;
                     const expr = pair[1] as model.TokenExpr;
-                    if (model.isTokenExprSatisfiable(expr) !== expected) {
-                        recorder.fail((expected
-                            ? "Expression not satisfiable: "
-                            : "Expression satisfiable: ")
-                            + JSON.stringify(expr));
+                    if (model.isTokenExprSatisfiable(expr) !== isSatisfiable) {
+                        recorder.fail((isSatisfiable
+                            ? "Expression should be satisfiable: "
+                            : "Expression should not be satisfiable: ")
+                            + util.inspect(expr, { colors: true, depth: 100 }));
                         return;
                     }
                 }
