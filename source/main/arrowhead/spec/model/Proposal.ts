95a5edf228fa922c7a759f7a26d921d74f910ff7
Emanuel Palm <emanuel.palm@ltu.se>
2018-05-28 16:06:41 +0200

    Update Proposal

--- source/main/arrowhead/spec/model/Proposal.ts
+++ source/main/arrowhead/spec/model/Proposal.ts
@@ -7,12 +7,12 @@ export interface Proposal {
     /**
      * The moment in time when this `Proposal` becomes acceptable.
      */
-    baseline: Date,
+    baseline?: Date,
 
     /**
      * The moment in time when this `Proposal` ceases to be acceptable.
      */
-    deadline: Date,
+    deadline?: Date,
 
     /**
      * A description of what tokens are desired.
@@ -34,12 +34,44 @@ export interface Proposal {
  * @returns Whether `proposal` is acceptable.
  */
 export function isProposalAcceptable(proposal: Proposal, fudgeInMs = 300) {
-    if (!isProposalQualified(proposal) || !isProposalSatisfiable(proposal)) {
+    return isProposalTimely(proposal, fudgeInMs)
+        && isProposalQualified(proposal)
+        && isProposalSatisfiable(proposal);
+}
+
+/**
+ * Checks whether given `Proposal` can be rejected.
+ *
+ * All acceptable proposals can be rejected, but only rejectable proposals with
+ * a passed baseline can be accepted.
+ *
+ * @param proposal Checked `Proposal`.
+ * @param fudgeInMs Permitted error when comparing current time to `Proposal`
+ *                  `baseline` and `deadline`. Defaults to 300 ms.
+ * @returns Whether `proposal` is acceptable.
+ */
+export function isProposalRejectable(proposal: Proposal, fudgeInMs = 300) {
+    const now = Date.now();
+    if (proposal.deadline && now >= (proposal.deadline.getTime() + fudgeInMs)) {
         return false;
     }
+    return isProposalQualified(proposal)
+        && isProposalSatisfiable(proposal);
+}
+
+/**
+ * Checks whether given `Proposal` is timely, meaning the current time is after
+ * its `baseline` and before its `deadline`.
+ *
+ * @param proposal Checked `Proposal`.
+ * @param fudgeInMs Permitted error when comparing current time to `Proposal`
+ *                  `baseline` and `deadline`. Defaults to 300 ms.
+ * @returns Whether `proposal` is timely.
+ */
+export function isProposalTimely(proposal: Proposal, fudgeInMs = 300) {
     const now = Date.now();
-    return now >= (proposal.baseline.getTime() - fudgeInMs)
-        && now < (proposal.deadline.getTime() + fudgeInMs);
+    return proposal.baseline && now >= (proposal.baseline.getTime() - fudgeInMs)
+        && proposal.deadline && now < (proposal.deadline.getTime() + fudgeInMs);
 }
 
 /**
@@ -54,12 +86,17 @@ export function isProposalQualified(proposal: Proposal): boolean {
 }
 
 /**
- * Checks whether given `Proposal` is a satisfiable Boolean expression.
+ * Checks whether given `Proposal` is satisfiable.
  *
  * @param proposal Checked `Proposal`
  * @returns Whether `proposal` is valid.
  */
 export function isProposalSatisfiable(proposal: Proposal): boolean {
+    if (proposal.baseline && proposal.deadline) {
+        if (proposal.baseline > proposal.deadline) {
+            return false;
+        }
+    }
     return isTokenExprSatisfiable(proposal.want)
         && isTokenExprSatisfiable(proposal.give);
 }
